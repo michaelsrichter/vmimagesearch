e0172a3f95f1438f7381126910d68dcbf37e2b8f
michaelsrichter@hotmail.com <michaelsrichter@hotmail.com>
2015-06-08 16:54:39 -0400

    Initial commit

---
+++ vmimagesearch/src/vmimagesearch.website/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNet.Mvc;
+
+namespace vmimagesearch.website.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
